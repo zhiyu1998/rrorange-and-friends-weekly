e53316941cefb0bdd4088dc5dfdd5664d6e075b6
zhiyu1998 <542716863@qq.com>
2024-06-22 15:32:45 +0800

    ✨ feat: 推送《第002期 - 珠海长隆》

--- docs/.vitepress/utils/createSideBar.ts
+++ docs/.vitepress/utils/createSideBar.ts
@@ -4,6 +4,7 @@ export function createSideBarZH() {
       text: '2024年',
       collapsed: true,
       items: [
+        { text: '第002期 - 珠海长隆', link: '/posts/2024/002' },
         { text: '第001期 - 田野孤屋', link: '/posts/2024/001' },
         { text: '第000期 - 新的开始', link: '/posts/2024/000' },
       ]
@@ -18,6 +19,7 @@ export function createSideBarEN() {
       text: 'The Year 2024',
       collapsed: true,
       items: [
+        { text: '002: Chimelong Ocean Kingdom', link: '/posts/2024/002' },
         { text: '001: Solitary House in the Fields', link: '/en/posts/2024/001' },
         { text: '000: A New Beginning', link: '/en/posts/2024/000' },
       ]
