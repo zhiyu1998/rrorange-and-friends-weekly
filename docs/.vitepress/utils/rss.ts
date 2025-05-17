09578f3e6884de5b2ce05cb21f0972fbe1e662ab
zhiyu1998 <542716863@qq.com>
2025-05-17 12:09:49 +0800

    ✨ feat: 推送周刊《第031期 - 金汤力儿》

--- docs/.vitepress/utils/rss.ts
+++ docs/.vitepress/utils/rss.ts
@@ -8,7 +8,7 @@ const hostname = "https://rrorangeandfriends.site";
 export async function createRssFileZH(config: SiteConfig) {
   const feed = new Feed({
     title: "大橘和朋友们的周刊",
-    description: "《大橘和朋友们的周刊》：分享日常冲浪互联网看到好玩的网站、app应用、资源分享、效率软件工具集等",
+    description: "《大橘和朋友们的周刊》：分享日常冲浪互联网看到好玩的网站、app应用、资源分享、效率软件工具集等。feedId:41680673211708416+userId:8Jv563DtwLFmrvznJtU90Or0LgSVjRhM",
     id: hostname,
     link: hostname,
     language: "zh-Hans",
